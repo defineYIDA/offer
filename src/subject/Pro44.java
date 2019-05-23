f7d4d6e33841037efe2bbf6f4530f4c9922379d9
1158558425 <1610130044@vip.henu.edu.cn>
2019-05-23 20:48:15 +0800

    数字序列中的某一位数 把数组排列最小的数

---
+++ src/subject/Pro44.java
@@ -0,0 +1,65 @@
+package subject;
+
+/**
+ * @Author: zl
+ * @Date: 2019/5/23 9:59
+ */
+public class Pro44 {
+
+    public static void main(String[] var) {
+/*        String nums = "";
+        for (int i = 0; i < 100; i++) {
+            nums += String.valueOf(i);
+        }*/
+        System.out.println(digitAtIndex(8));
+        System.out.println(digitAtIndex(11));
+        System.out.println(digitAtIndex(12));
+        System.out.println(digitAtIndex(13));
+        System.out.println(digitAtIndex(19));
+        System.out.println(digitAtIndex(198));
+    }
+    /**
+     * 题目44：数字序列（0123...规律的无限序列）中的某一位数
+     *           思路：因为序列是有规律的，所以一位的占10位(0~9)
+     *                     两位的占180(10~99),三位的占2700(100~999)
+     * @param index
+     * @return
+     */
+    public static int digitAtIndex(int index) {
+        if (index < 0) {
+            return -1;
+        }
+        int digit = 1;//当前位数
+        while (true) {
+            //求当前位数的个数
+            int number = countOf(digit);
+            if (index < digit * number) {
+                return indexOf(digit, index);
+            }
+            index -= digit * number;
+            digit++;
+        }
+    }
+
+    public static int countOf(int digit) {
+        if (digit == 1) {
+            return 10;
+        }
+        return (int) Math.pow(10, digit - 1) * 9;
+    }
+    public static int indexOf(int digit, int index) {
+        int num = beginNum(digit) + index / digit;//找到index对应的数字是那个，则index对应的数就在这里面
+        int indexFromNum = digit - index % digit;
+        for (int i = 1; i < indexFromNum; i++) {
+            num /= 10;
+        }
+        return num % 10;
+    }
+    public static int beginNum(int digit) {
+        if (digit == 1) {
+            return 0;
+        }else {
+            return (int)Math.pow(10, digit - 1);
+        }
+    }
+}
---
+++ src/subject/Pro45.java
@@ -0,0 +1,84 @@
+package subject;
+
+import java.util.Comparator;
+
+/**
+ * @Author: zl
+ * @Date: 2019/5/23 11:32
+ */
+public class Pro45 {
+    /**
+     *题45，把数组排成最小的数
+     *    实现自定义的比较规则：nm 和 mn的关系，然后运用快排进行排序
+     *    使用字符串或数组来解决大数问题
+     *快排总结：
+     *           1）重要条件：left < right---------1
+     *           2）找基准点：a:以start索引位的元素为基准点，b:从start~end的范围内radom
+     *           3）迭代：先右(从右找到第一个小于base的index，左移)后左(从左找到第一个大于base的index，右移)
+     *           4）交换：对应index的元素值(满足条件1的情况下)
+     *           5）交换：base和left的元素值
+     *           6）递归：<start~left-1> <left+1~end>
+     */
+    public String PrintMinNumber(int [] numbers) {
+        if (numbers.length == 0) {
+            return "";
+        }
+        MComparator com = new MComparator();
+        quickSort(numbers, 0, numbers.length - 1, com);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i : numbers) {
+            sb.append(String.valueOf(i));
+        }
+
+        return sb.toString();
+    }
+
+    private void quickSort(int [] numbers, int start, int end, MComparator com) {
+        if (start >= end) {
+            return ;
+        }
+        int base = numbers[start];//基准点
+        int left = start;
+        int right = end;
+        while (left < right) {
+            /*
+            //right 左移 找到第一个小于基准值的位置
+            while (left < right && numbers[right] >= base) {
+                right--;
+            }
+            //left 右移
+            while (left < right && numbers[left] <= base) {
+                left++;
+            }
+            */
+            while (left < right && com.compare(numbers[right], base) >= 0) {
+                right--;
+            }
+            //left 右移
+            while (left < right && com.compare(numbers[left], base) <= 0) {
+                left++;
+            }
+            if (left < right) {
+                int temp = numbers[left];
+                numbers[left] = numbers[right];
+                numbers[right] = temp;
+            }
+        }
+        numbers[start] = numbers[left];
+        numbers[left] = base;
+        quickSort(numbers, start, left - 1, com);
+        quickSort(numbers, start + 1, right, com);
+    }
+    private static class MComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            String num1 = o1.toString();
+            String num2 = o2.toString();
+            String s1 = num1 + num2;
+            String s2 = num2 + num1;
+            return s1.compareTo(s2);
+        }
+    }
+
+}
