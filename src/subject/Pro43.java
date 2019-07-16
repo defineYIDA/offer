68cacbed1054bef07f0b0596b1a76cb5a43c4fe8
1158558425 <1610130044@vip.henu.edu.cn>
2019-07-16 10:51:01 +0800

    nunberOf1

--- src/subject/Pro43.java
+++ src/subject/Pro43.java
@@ -24,4 +24,52 @@ public class Pro43 {
             return num;
         }
     }
+    //---
+    public int countDigitOne(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        //将数字转化为数组处理
+        String val = n + "";
+        int len = val.length();
+        int[] nums = new int[len];
+        for (int i = 0; i < len; i++) {
+            nums[i] = val.charAt(i) - '0';
+        }
+        return numberOf1(nums, 0);
+    }
+    private int numberOf1(int[] nums, int index) {
+        if (nums == null || index >= nums.length || index < 0) {
+            return 0;
+        }
+        //每一次都计算最高位上的1的个数
+        int first = nums[index];
+        int len = nums.length - index;
+        //只有一位，且不为0
+        if (len == 1 && first > 0) {
+            return 1;
+        }
+        int c1 = 0;
+        //最高位大于1，如21345，最高位的1出现在[10000, 19999]中出现次数为，10^4
+        if (first > 1) {
+            c1 = (int)Math.pow(10, len - 1);
+        }
+        //最高位等于1，如12345，最高位1出现在[10000， 12345]中出现次数为，2345 + 1
+        else if (first == 1) {
+            c1 = atoi(nums, index + 1) + 1;
+        }
+        //计算其他位数中1的个数,[1346, 21345],进一步分为两部分：[1345, 11345] [11345, 21345]
+        //每一部分10000个数，排列组合：2 * 4 * C10取1^3 = 8000
+        int c2 = first * (len - 1) * (int)Math.pow(10, len - 2);
+        //计算[0, 1345] ,这一部分使用递归进行解决
+        int c3 = numberOf1(nums, index + 1);
+        return c1 + c2 + c3;
+    }
+    private int atoi(int[] nums, int index) {
+        int res = 0;
+        for (int i = index; i < nums.length; i++) {
+            res = res * 10 + nums[i];
+        }
+        return res;
+    }
 }
