991366865c05d128d25f6cbec66ff3c937e5e321
1158558425 <1610130044@vip.henu.edu.cn>
2019-08-27 23:05:12 +0800

    AQS实现的自定义独占锁和共享锁

---
+++ src/thread/AQS/DoubleLock.java
@@ -0,0 +1,52 @@
+package thread.AQS;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @Author: zl
+ * @Date: 2019/8/27 22:51
+ */
+public class DoubleLock {
+    private static class Sync extends AbstractQueuedSynchronizer {
+        public Sync() {
+            super();
+            //初始化状态
+            setState(2);
+        }
+
+        @Override
+        protected int tryAcquireShared(int arg) {
+            while (true) {
+                int cur = getState();
+                int next = getState() - arg;
+                //因为存在并发线程更改同步状态，所以需要cas确保线程安全
+                if (compareAndSetState(cur, next)) {
+                    //返回的是状态量
+                    return next;
+                }
+            }
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int arg) {
+            while (true) {
+                int cur = getState();
+                int next = getState() + arg;
+                if (compareAndSetState(cur, next)) {
+                    return true;
+                }
+            }
+        }
+
+        private Sync sync = new Sync();
+
+        public void lock() {
+            sync.acquireShared(1);
+        }
+
+        public void unlock() {
+            sync.releaseShared(1);
+        }
+
+    }
+}
---
+++ src/thread/AQS/MyLock.java
@@ -0,0 +1,37 @@
+package thread.AQS;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @Author: zl
+ * @Date: 2019/8/27 1:35
+ */
+public class MyLock {
+    private static class Sync extends AbstractQueuedSynchronizer {
+        @Override
+        protected boolean tryAcquire(int arg) {
+            return compareAndSetState(0, 1);
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+    }
+
+    private Sync sync = new Sync();
+
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    public void unlock() {
+        sync.release(1);
+    }
+}
